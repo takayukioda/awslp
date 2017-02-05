d0974322cea605171ac77a97379ea46f9429e09e
takayukioda <takayuki@odashi.me>
2017-02-05 11:31:34 +0900

    :+1: Initialize code with todo

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const awslp = require("./lib/awslp");
+
+console.log(awslp);
+const todo = [
+  "awslp s3 create <bucket-name>",
+  "awslp s3 set-policy <bucket-name>",
+  "awslp s3 sync <directory> <bucket-name>",
+  "awslp cf create <bucket-name>",
+  "awslp cf create-invalidation <bucket-name>",
+  "awslp deploy"
+];
+
+console.log("ToDo", todo);
